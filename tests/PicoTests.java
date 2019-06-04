0b098aad6a0c38df0dfeb1abbf3389f7cd4dec81
Fran Day <franday_7@hotmail.com>
2019-06-04 20:56:36 -0300

    Archivo HerramientasTests separado
    Separado en PicoTests y Hacha

--- tests/PicoTests.java
+++ tests/PicoTests.java
@@ -3,39 +3,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class HerramientasTest {
-
-    @Test
-    public void testHachaDeMaderaCon100Durabilidad(){
-        Constructor constructor = new Constructor();
-        Herramienta unHachaDeMadera = constructor.construirHachaDeMadera();
-
-        assertEquals(100, unHachaDeMadera.getDurabilidad());
-    }
-
-    @Test
-    public void testHachaDeMaderaCon2Fuerza(){
-        Constructor constructor = new Constructor();
-        Herramienta unHachaDeMadera = constructor.construirHachaDeMadera();
-
-        assertEquals(2, unHachaDeMadera.getFuerza());
-    }
-
-    @Test
-    public void testHachaDeMetalCon400Durabilidad(){
-        Constructor constructor = new Constructor();
-        Herramienta unHachaDeMetal = constructor.construirHachaDeMetal();
-
-        assertEquals(400, unHachaDeMetal.getDurabilidad());
-    }
-
-    @Test
-    public void testHachaDeMetalCon10Fuerza(){
-        Constructor constructor = new Constructor();
-        Herramienta unHachaDeMetal = constructor.construirHachaDeMetal();
-
-        assertEquals(10, unHachaDeMetal.getFuerza());
-    }
+public class PicoTests {
 
     @Test
     public void testPicoDeMaderaCon100Durabilidad(){
