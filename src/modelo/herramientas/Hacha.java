be99dc7362a9b34823bc34e3aa14be0e3884e5de
tizziana <51216329+tizziana@users.noreply.github.com>
2019-06-05 16:49:30 -0300

    Hacha golpea materiales

--- src/modelo/herramientas/Hacha.java
+++ src/modelo/herramientas/Hacha.java
@@ -9,7 +9,22 @@ public class Hacha extends Herramienta {
         this.fuerza = fuerza;
         this.desgaste = desgaste;
     }
-    public UsarHacha(){
-       
+
+    public void golpear(Madera unaMadera){
+        this.durabilidad = this.desgastar(this.durabilidad);
+        unaMadera.disminuirDurabilidad(this.fuerza);
     }
+
+    public void golpear(Piedra unaPiedra){
+        this.durabilidad = this.desgastar(this.durabilidad);
+    }
+
+    public void golpear(Metal unMetal){
+        this.durabilidad = this.desgastar(this.durabilidad);
+    }
+
+    public void golpear(Diamante unDiamante){
+        this.durabilidad = this.desgastar(this.durabilidad);
+    }
+
 }
