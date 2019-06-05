fd39fc0718f728cda5531069a55f38b9f38c8b8a
tizziana <51216329+tizziana@users.noreply.github.com>
2019-06-05 17:38:38 -0300

    Herramientas con sus pruebas respectivas

--- tests/HachaTest.java
+++ tests/HachaTest.java
@@ -37,4 +37,130 @@ public class HachaTest {
         assertEquals(10, unHachaDeMetal.getFuerza());
     }
 
+
+    @Test
+    public void testHachaDeMaderaGolpeaMadera(){
+        Constructor unConstructor = new Constructor();
+        Herramienta unHachaDeMadera = unConstructor.construirHachaDeMadera();
+        Material unaMadera = new Madera();
+
+        unHachaDeMadera.golpear(unaMadera);
+
+        assertEquals(98, unHachaDeMadera.getDurabilidad());
+    }
+
+    @Test
+    public void testHachaDeMaderaGolpeaPiedra(){
+        Constructor unConstructor = new Constructor();
+        Herramienta unHachaDeMadera = unConstructor.construirHachaDeMadera();
+        Material unaPiedra = new Piedra();
+
+        unHachaDeMadera.golpear(unaPiedra);
+
+        assertEquals(98, unHachaDeMadera.getDurabilidad());
+    }
+
+    @Test
+    public void testHachaDeMaderaGolpeaMetal(){
+        Constructor unConstructor = new Constructor();
+        Herramienta unHachaDeMadera = unConstructor.construirHachaDeMadera();
+        Material unMetal= new Metal();
+
+        unHachaDeMadera.golpear(unMetal);
+
+        assertEquals(98, unHachaDeMadera.getDurabilidad());
+    }
+
+    @Test
+    public void testHachaDeMaderaGolpeaDiamante(){
+        Constructor unConstructor = new Constructor();
+        Herramienta unHachaDeMadera = unConstructor.construirHachaDeMadera();
+        Material unDiamante= new Diamante();
+
+        unHachaDeMadera.golpear(unDiamante);
+
+        assertEquals(98, unHachaDeMadera.getDurabilidad());
+    }
+
+    public void testHachaDePiedraGolpeaMadera(){
+        Constructor unConstructor = new Constructor();
+        Herramienta unHachaDePiedra = unConstructor.construirHachaDePiedra();
+        Material unaMadera = new Madera();
+
+        unHachaDePiedra.golpear(unaMadera);
+
+        assertEquals(195, unHachaDePiedra.getDurabilidad());
+    }
+
+    public void testHachaDePiedraGolpeaPiedra(){
+        Constructor unConstructor = new Constructor();
+        Herramienta unHachaDePiedra = unConstructor.construirHachaDePiedra();
+        Material unaPiedra = new Piedra();
+
+        unHachaDePiedra.golpear(unaPiedra);
+
+        assertEquals(195, unHachaDePiedra.getDurabilidad());
+    }
+
+    public void testHachaDePiedraGolpeaMetal(){
+        Constructor unConstructor = new Constructor();
+        Herramienta unHachaDePiedra = unConstructor.construirHachaDePiedra();
+        Material unMetal= new Metal();
+
+        unHachaDePiedra.golpear(unMetal);
+
+        assertEquals(195, unHachaDePiedra.getDurabilidad());
+    }
+
+    public void testHachaDePiedraGolpeaDiamante(){
+        Constructor unConstructor = new Constructor();
+        Herramienta unHachaDePiedra = unConstructor.construirHachaDePiedra();
+        Material unDiamante = new Diamante();
+
+        unHachaDePiedra.golpear(unDiamante);
+
+        assertEquals(195, unHachaDePiedra.getDurabilidad());
+    }
+
+    public void testHachaDeMetalGolpeaMadera(){
+        Constructor unConstructor = new Constructor();
+        Herramienta unHachaDeMetal = unConstructor.construirHachaDeMetal();
+        Material unaMadera = new Madera();
+
+        unHachaDeMetal.golpear(unaMadera);
+
+        assertEquals(390, unHachaDeMetal.getDurabilidad());
+    }
+
+    public void testHachaDeMetalGolpeaPiedra(){
+        Constructor unConstructor = new Constructor();
+        Herramienta unHachaDeMetal = unConstructor.construirHachaDeMetal();
+        Material unaPiedra= new Piedra();
+
+        unHachaDeMetal.golpear(unaPiedra);
+
+        assertEquals(390, unHachaDeMetal.getDurabilidad());
+    }
+
+    public void testHachaDeMetalGolpeaMetal(){
+        Constructor unConstructor = new Constructor();
+        Herramienta unHachaDeMetal = unConstructor.construirHachaDeMetal();
+        Material unMetal= new Metal();
+
+        unHachaDeMetal.golpear(unMetal);
+
+        assertEquals(390, unHachaDeMetal.getDurabilidad());
+    }
+
+    public void testHachaDeMetalGolpeaDiamante(){
+        Constructor unConstructor = new Constructor();
+        Herramienta unHachaDeMetal = unConstructor.construirHachaDeMetal();
+        Material unDiamante = new Diamante();
+
+        unHachaDeMetal.golpear(unDiamante);
+
+        assertEquals(390, unHachaDeMetal.getDurabilidad());
+    }
+
 }
+
--- tests/PicoTest.java
+++ tests/PicoTest.java
@@ -52,4 +52,181 @@ public class PicoTest {
 
         assertEquals(20, unPicoFino.getFuerza());
     }
+
+    @Test
+    public void testPicoDeMaderaGolpeaMadera(){
+        Constructor unConstructor = new Constructor();
+        Herramienta unPicoDeMadera = unConstructor.construirPicoDeMadera();
+        Material unaMadera = new Madera();
+
+        unPicoDeMadera.golpear(unaMadera);
+
+        assertEquals(98, unPicoDeMadera.getDurabilidad());
+    }
+
+    @Test
+    public void testPicoDeMaderaGolpeaPiedra(){
+        Constructor unConstructor = new Constructor();
+        Herramienta unPicoDeMadera = unConstructor.construirPicoDeMadera();
+        Material unaPiedra = new Piedra();
+
+        unPicoDeMadera.golpear(unaPiedra);
+
+        assertEquals(98, unPicoDeMadera.getDurabilidad());
+    }
+
+    @Test
+    public void testPicoDeMaderaGolpeaMetal(){
+        Constructor unConstructor = new Constructor();
+        Herramienta unPicoDeMadera = unConstructor.construirPicoDeMadera();
+        Material unMetal = new Metal();
+
+        unPicoDeMadera.golpear(unMetal);
+
+        assertEquals(98, unPicoDeMadera.getDurabilidad());
+    }
+
+    @Test
+    public void testPicoDeMaderaGolpeaDiamante(){
+        Constructor unConstructor = new Constructor();
+        Herramienta unPicoDeMadera = unConstructor.construirPicoDeMadera();
+        Material unDiamante = new Diamante();
+
+        unPicoDeMadera.golpear(unDiamante);
+
+        assertEquals(98, unPicoDeMadera.getDurabilidad());
+    }
+
+    @Test
+    public void testPicoDePiedraGolpeaMadera(){
+        Constructor unConstructor = new Constructor();
+        Herramienta unPicoDePiedra = unConstructor.construirPicoDePiedra();
+        Material unaMadera = new Madera();
+
+        unPicoDePiedra.golpear(unaMadera);
+
+        assertEquals( (200 - (4 / (1.5)) ), unPicoDePiedra.getDurabilidad());
+    }
+
+    @Test
+    public void testPicoDePiedraGolpeaPiedra(){
+        Constructor unConstructor = new Constructor();
+        Herramienta unPicoDePiedra = unConstructor.construirPicoDePiedra();
+        Material unaPiedra = new Piedra();
+
+        unPicoDePiedra.golpear(unaPiedra);
+
+        assertEquals( (200 - (4 / (1.5)) ), unPicoDePiedra.getDurabilidad());
+    }
+
+    @Test
+    public void testPicoDePiedraGolpeaMetal(){
+        Constructor unConstructor = new Constructor();
+        Herramienta unPicoDePiedra = unConstructor.construirPicoDePiedra();
+        Material unMetal = new Metal();
+
+        unPicoDePiedra.golpear(unMetal);
+
+        assertEquals( (200 - (4 / (1.5)) ), unPicoDePiedra.getDurabilidad());
+    }
+
+    @Test
+    public void testPicoDePiedraGolpeaDiamante(){
+        Constructor unConstructor = new Constructor();
+        Herramienta unPicoDePiedra = unConstructor.construirPicoDePiedra();
+        Material unDiamante = new Diamante();
+
+        unPicoDePiedra.golpear(unDiamante);
+
+        assertEquals( (200 - (4 / (1.5)) ), unPicoDePiedra.getDurabilidad());
+    }
+
+    @Test
+    public void testPicoDeMetalGolpeaMadera(){
+        Constructor unConstructor = new Constructor();
+        Herramienta unPicoDeMetal = unConstructor.construirPicoDeMetal();
+        Material unaMadera = new Madera();
+
+        unPicoDeMetal.golpear(unaMadera);
+
+        assertEquals(400, unPicoDeMetal.getDurabilidad());
+    }
+
+    @Test
+    public void testPicoDeMetalGolpeaPiedra(){
+        Constructor unConstructor = new Constructor();
+        Herramienta unPicoDeMetal = unConstructor.construirPicoDeMetal();
+        Material unaPiedra = new Piedra();
+
+        unPicoDeMetal.golpear(unaPiedra);
+
+        assertEquals(400, unPicoDeMetal.getDurabilidad());
+    }
+
+    @Test
+    public void testPicoDeMetalGolpeaMetal(){
+        Constructor unConstructor = new Constructor();
+        Herramienta unPicoDeMetal = unConstructor.construirPicoDeMetal();
+        Material unMetal = new Metal();
+
+        unPicoDeMetal.golpear(unMetal);
+
+        assertEquals(400, unPicoDeMetal.getDurabilidad());
+    }
+
+    @Test
+    public void testPicoDeMetalGolpeaDiamante(){
+        Constructor unConstructor = new Constructor();
+        Herramienta unPicoDeMetal = unConstructor.construirPicoDeMetal();
+        Material unDiamante = new Diamante();
+
+        unPicoDeMetal.golpear(unDiamante);
+
+        assertEquals(400, unPicoDeMetal.getDurabilidad());
+    }
+
+    @Test
+    public void testPicoFinoGolpeaMaderaNoReduceSuDurabilidad(){
+        Constructor unConstructor = new Constructor();
+        Herramienta unPicoFino = unConstructor.construirPicoFino();
+        Material unaMadera = new Madera();
+
+        unPicoFino.golpear(unaMadera);
+
+        assertEquals(1000, unPicoFino.getDurabilidad());
+    }
+
+    @Test
+    public void testPicoFinoGolpeaPiedraNoReduceSuDurabilidad(){
+        Constructor unConstructor = new Constructor();
+        Herramienta unPicoFino = unConstructor.construirPicoFino();
+        Material unaPiedra = new Piedra();
+
+        unPicoFino.golpear(unaPiedra);
+
+        assertEquals(1000, unPicoFino.getDurabilidad());
+    }
+
+    @Test
+    public void testPicoFinoGolpeaMetalNoReduceSuDurabilidad(){
+        Constructor unConstructor = new Constructor();
+        Herramienta unPicoFino = unConstructor.construirPicoFino();
+        Material unMetal = new Metal();
+
+        unPicoFino.golpear(unMetal);
+
+        assertEquals(1000, unPicoFino.getDurabilidad());
+    }
+
+    @Test
+    public void testPicoFinoGolpeaDiamante(){
+        Constructor unConstructor = new Constructor();
+        Herramienta unPicoFino = unConstructor.construirPicoFino();
+        Material unDiamante = new Diamante();
+
+        unPicoFino.golpear(unDiamante);
+
+        assertEquals( (1000 - (1000 * 0.1)), unPicoFino.getDurabilidad());
+    }
+
 }
