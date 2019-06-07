af4b0aefdb53ea5ac021e7bf2c8719309ff80d0b
Fran Day <franday_7@hotmail.com>
2019-06-07 16:32:48 -0300

    Recetas agregada y jugador modificado
    Es mi planteo de las recetas, se puede cambiar. Faltaria ver como hacer una herramienta nueva

---
+++ src/modelo/construccion/Recetas.java
@@ -0,0 +1,52 @@
+package modelo.construccion;
+
+import modelo.excepciones.*;
+import modelo.herramientas.*;
+import modelo.materiales.*;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class Recetas {
+    private final static int TAMANO_ARRAY = 9;
+    public static HashMap<ArrayList<Material>, Herramienta> Recetas = new HashMap<>();
+
+    public void agregarReceta(Material material1, Material material2, Material material3, Material material4,
+                              Material material5 , Material material6,  Material material7,  Material material8,
+                              Material material9, Herramienta herramienta){
+
+        Material[] patronDeReceta = new Material[]{material1, material2, material3, material4, material5,
+                                                   material6, material7, material8, material9};
+
+        ArrayList<Material> inputReceta = new ArrayList<>(Arrays.asList(patronDeReceta));
+        Recetas.put(inputReceta, herramienta);
+    }
+
+    //TODO falta contruir herramienta
+    public Herramienta construirHerramienta(ArrayList<Material> receta){
+        if(Recetas.containsKey(receta)){
+            return Recetas.get(receta);
+        }
+        return null;
+    }
+
+    public Herramienta buscarReceta(ArrayList<Material> nuevaReceta){
+        for(ArrayList<Material> receta : Recetas.keySet()){
+            if(compararRecetas(nuevaReceta, receta)){
+                return Recetas.get(receta);
+            }
+        }
+        throw new RecetaInvalida();
+    }
+
+    private boolean compararRecetas(ArrayList<Material> receta1, ArrayList<Material> receta2){
+        for(int i = 0; i < TAMANO_ARRAY; i++){
+            if(receta1.get(i).getClass() != receta2.get(i).getClass()){
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
--- src/modelo/jugador/Jugador.java
+++ src/modelo/jugador/Jugador.java
@@ -1,18 +1,30 @@
 package modelo.jugador;
 
-import java.util.ArrayList;
+import java.util.*;
 import modelo.herramientas.*;
+import modelo.materiales.*;
 
 public class Jugador {
     private Herramienta herramientaEquipada;
-    private ArrayList inventario;
+    private ArrayList inventarioMateriales;
+    private ArrayList inventarioHerramientas;
 
     public Jugador(){
         Constructor unConstructor = new Constructor();
         herramientaEquipada = unConstructor.construirHachaDeMadera();
+        inventarioHerramientas = new ArrayList<Herramienta>();
+        inventarioMateriales = new ArrayList<Material>();
     }
 
     public Herramienta getHerramientaEquipada() {
         return this.herramientaEquipada;
+    }
+
+    public ArrayList getInventarioMateriales(){
+        return inventarioMateriales;
+    }
+
+    public ArrayList getInventarioHerramientas(){
+        return inventarioHerramientas;
     }
 }
