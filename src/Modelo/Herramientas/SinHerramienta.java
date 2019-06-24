db3d79faeef5677d8e5a867d4126da712f75c023
Unknown <asofi06@hotmail.com>
2019-06-24 00:57:12 -0300

    agregar SinHerrramienta a Herramienta

--- src/Modelo/Herramientas/SinHerramienta.java
+++ src/Modelo/Herramientas/SinHerramienta.java
@@ -1,4 +1,27 @@
 package Modelo.Herramientas;
+import Modelo.Materiales.Diamante;
+import Modelo.Materiales.Madera;
+import Modelo.Materiales.Metal;
+import Modelo.Materiales.Piedra;
 
 public class SinHerramienta extends Herramienta {
+    @Override
+    public void recolectar(Madera unaMadera) {
+
+    }
+
+    @Override
+    public void recolectar(Piedra unaPiedra) {
+
+    }
+
+    @Override
+    public void recolectar(Metal unMetal) {
+
+    }
+
+    @Override
+    public void recolectar(Diamante unDiamante) {
+
+    }
 }
