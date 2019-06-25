7f70e2f8bc180244dcf97ea10768281a5837598c
Fran Day <franday_7@hotmail.com>
2019-06-25 18:09:53 -0300

    Display de inventario mejorado

--- src/Vista/InventarioViewer.java
+++ src/Vista/InventarioViewer.java
@@ -2,20 +2,18 @@ package Vista;
 
 import Modelo.Materiales.*;
 import Modelo.MesaDeCrafteo.MesaDeCrafteo;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.GridPane;
 
 import java.util.ArrayList;
 
-public class InventarioViewer extends VBox {
+public class InventarioViewer extends GridPane {
     private static InventarioViewer instance;
     private ArrayList<Material> Materiales;
     private MesaDeCrafteo mesaDeCrafteo;
     private double OffsetX = 290;
-    private double OffsetY = 77;
+    private double OffsetY = 363;
+    private static int TAM_INVENTARIO = 9;
     private static int TAM_BOTON = 32;
     private double Hgap = 4;
     private double Vgap = 4;
@@ -29,32 +27,19 @@ public class InventarioViewer extends VBox {
         this.mesaDeCrafteo = mesa;
         instance = this;
         configurarVentana();
-        HBox box = new HBox();
-        configurarHBox(box);
-        HBox box2 = new HBox();
-        configurarHBox(box2);
-        HBox box3 = new HBox();
-        configurarHBox(box3);
-        this.getChildren().addAll(box, box2, box3);
-
     }
 
     private void configurarVentana(){
-        this.setSpacing(Vgap);
-        this.setAlignment(Pos.CENTER);
+        this.setVgap(Vgap);
+        this.setHgap(Hgap);
         this.getStylesheets().add("Vista/styleInventario.css");
         this.setId("inventario");
     }
 
-    private void configurarHBox(HBox hbox){
-        hbox.setSpacing(Hgap);
-        hbox.setTranslateX(OffsetX);
-        hbox.setTranslateY(OffsetY);
-        hbox.setPadding(Insets.EMPTY);
-    }
-
     private void configurarBoton(Button unBoton, String Id){
         unBoton.setMinSize(TAM_BOTON, TAM_BOTON);
+        unBoton.setTranslateX(OffsetX);
+        unBoton.setTranslateY(OffsetY);
         unBoton.setId(Id);
     }
 
